86ca839bdb574eee023f457d5ce05f43fac806fe
Sundar Thambidurai <126937777+SundarFullStack@users.noreply.github.com>
2023-08-08 20:23:16 +0530

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -158,7 +158,7 @@ router.post("/sendpasswordlink", async (req, res) => {
         from: process.env.EMAIL,
         to: email,
         subject: "Sending Email For password Reset",
-        text: `This Link Valid For 2 MINUTES http://localhost:3001/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`,
+        text: `This Link Valid For 2 MINUTES https://bespoke-custard-7d7688.netlify.app/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`,
       };
 
       transporter.sendMail(mailOptions, (error, info) => {
